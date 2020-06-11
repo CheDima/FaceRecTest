9057cdeb973a616cbc68b7b280e29ddabdc75c56
DimaChe <chde@inbox.ru>
2020-06-11 17:32:21 +0200

    Just test

--- src/com/demo/facedetect/FaceDetection.java
+++ src/com/demo/facedetect/FaceDetection.java
@@ -5,11 +5,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
@@ -20,38 +16,21 @@ import com.demo.utils.Utils;
 public class FaceDetection {
 
 	public static String basePath=System.getProperty("user.dir");
-	public static String classifierPath1=basePath+"\\src\\resources\\FaceDetection\\haarcascade_frontalface_alt.xml";
+	public static String classifierPath1=basePath+"\\src\\resources\\FaceDetection\\parojos.xml";
 	public static String inpImgFilename=basePath+"\\src\\resources\\FaceDetection\\input.jpg";
 	public static String opImgFilename=basePath+"\\src\\resources\\FaceDetection\\output.jpg";
+    private CascadeClassifier faceCascade = new CascadeClassifier();
 
+    public FaceDetection() {
+        faceCascade.load(classifierPath1);
+    }
 
-
-	public static void main(String[] args) {
-		try {
-			System.loadLibrary("libopencv_java342");
-			System.out.println("Library loaded..");
-			Mat frame=Imgcodecs.imread(inpImgFilename, 1);
-			if (!frame.empty())
-			{
-				// face detection
-				detectAndDisplay(frame);
-				File outputfile = new File(opImgFilename);
-			    ImageIO.write(Utils.matToBufferedImage(frame), "jpg", outputfile);
-			    System.out.println("Done!!");
-			}
-		} catch (IOException e) {
-			System.out.println("Exception IO");
-			e.printStackTrace();
-		}
-	}
-	public static void detectAndDisplay(Mat frame) throws IOException
+    public void detectAndDisplay(Mat frame) throws IOException
 	{
 		MatOfRect faces = new MatOfRect();
 		Mat grayFrame = new Mat();
 		int absoluteFaceSize=0;
-		CascadeClassifier faceCascade=new CascadeClassifier();
-		
-		faceCascade.load(classifierPath1);
+
 		// convert the frame in gray scale
 		Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
 		// equalize the frame histogram to improve the result
@@ -71,9 +50,17 @@ public class FaceDetection {
 				
 		// each rectangle in faces is a face: draw them!
 		Rect[] facesArray = faces.toArray();
-		System.out.println("Number of faces detected = "+facesArray.length);
-		for (int i = 0; i < facesArray.length; i++)
-			Imgproc.rectangle(frame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 2);
+		if (facesArray.length == 1) {
+			Point center = new Point(facesArray[0].x + facesArray[0].width/2, facesArray[0].y - 30 );
+			Imgproc.circle(frame, center,  10, new Scalar(0, 255, 255));
+			Imgproc.circle(frame, center,  20, new Scalar(0, 255, 255));
+			Imgproc.circle(frame, center,  30, new Scalar(0, 255, 255));
+			Imgproc.line(frame, new Point(center.x -40, center.y), new Point(center.x + 40, center.y),  new Scalar(0, 255, 255));
+			Imgproc.line(frame, new Point(center.x , center.y - 40), new Point(center.x, center.y + 40),  new Scalar(0, 255, 255));
+
+		}
+		//for (int i = 0; i < facesArray.length; i++)
+			//Imgproc.rectangle(frame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 2);
 			
 	}
 
--- src/com/demo/facerecognition/FaceRecognitionEigenFaces.java
+++ src/com/demo/facerecognition/FaceRecognitionEigenFaces.java
@@ -1,69 +1,125 @@
-package com.demo.facerecognition;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+package com.demo.facerecognition;/*
+ * Captures the camera stream with OpenCV
+ * Search for the faces
+ * Display a circle around the faces using Java
+ */
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
+import javax.swing.*;
 
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
-import org.opencv.face.EigenFaceRecognizer;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.core.*;
+import org.opencv.core.Point;
 
-public class FaceRecognitionEigenFaces {
-	public static String basePath=System.getProperty("user.dir");
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
 
-	public static String csvFilePath=basePath+"\\src\\resources\\FaceRecognition\\TrainingData.txt";
-	public static void main(String[] args) {
-		System.out.println("Loading library..");
-		System.loadLibrary("libopencv_java342");
-		System.out.println("Library loaded!!");
-		ArrayList<Mat> images=new ArrayList<>();
-		ArrayList<Integer> labels=new ArrayList<>();
-		readCSV(csvFilePath,images,labels);
-		
-		Mat testSample=images.get(images.size()-1);
-		Integer testLabel=labels.get(images.size()-1);
-		images.remove(images.size()-1);
-		labels.remove(labels.size()-1);
-		MatOfInt labelsMat=new MatOfInt();
-		labelsMat.fromList(labels);
-		EigenFaceRecognizer efr=EigenFaceRecognizer.create();
-		System.out.println("Starting training...");
-		efr.train(images, labelsMat);
-		
-		int[] outLabel=new int[1];
-		double[] outConf=new double[1];
-		System.out.println("Starting Prediction...");
-		 efr.predict(testSample,outLabel,outConf);
-		 
-		System.out.println("***Predicted label is "+outLabel[0]+".***");
+import static org.opencv.imgproc.Imgproc.boundingRect;
 
-		System.out.println("***Actual label is "+testLabel+".***");
-		System.out.println("***Confidence value is "+outConf[0]+".***");
-
+class My_Panel extends JPanel{
+	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
+	// Create a constructor method
+	public My_Panel(){
+		super();
 	}
-	private static void readCSV(String csvFilePath2, ArrayList<Mat> images, ArrayList<Integer> labels)  {
-		BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader(csvFilePath2));
-		
-		String line;
-		while((line=br.readLine())!=null){
-			String[] tokens=line.split("\\;");
-			Mat readImage=Imgcodecs.imread(tokens[0], 0);
-			images.add(readImage);
-			labels.add(Integer.parseInt(tokens[1]));
-		}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+	/**
+	 * Converts/writes a Mat into a BufferedImage.
+	 *
+	 * @paramjjj Mat of type CV_8UC3 or CV_8UC1
+	 * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY
+	 */
+	public boolean MatToBufferedImage(Mat matBGR){
+		int width = matBGR.width(), height = matBGR.height(), channels = matBGR.channels() ;
+		byte[] sourcePixels = new byte[width * height * channels];
+		matBGR.get(0, 0, sourcePixels);
+		// create new image and get reference to backing data
+		image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+		return true;
 	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		if (this.image==null) return;
+		g.drawImage(this.image,10,10,this.image.getWidth(),this.image.getHeight(), null);
+		//g.drawString("This is my custom Panel!",10,20);
+	}
+}
+class processor {
+	int i=0;
+	static final int SENSITIVITY_VALUE = 30;
+	Mat previous = new Mat();
+	boolean first = true;
+	private CascadeClassifier face_cascade;
+	// Create a constructor method
+	public processor(){
+		face_cascade=new CascadeClassifier("D:/Projects/Java/ShootIt/target.xml");
+		if(face_cascade.empty())
+		{
+			System.out.println("--(!)Error loading A\n");
+			return;
+		}
+		else
+		{
+			System.out.println("Face classifier loooaaaaaded up");
+		}
+	}
+	public Mat detect(Mat inputframe){
 
+		Mat mDiff=new Mat();
+		Mat mGray=new Mat();
+		Mat mCont = new Mat();
+
+
+		//inputframe.copyTo(mGray);
+		Imgproc.cvtColor(inputframe, mGray, Imgproc.COLOR_BGR2GRAY);
+		//Imgproc.equalizeHist( mGray, mGray );
+
+		MatOfRect faces = new MatOfRect();
+		//inputframe.copyTo(mRgba);
+		//inputframe.copyTo(mGrey);
+		// Imgproc.cvtColor( mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
+		//Imgproc.equalizeHist( mGrey, mGrey );
+/*        face_cascade.detectMultiScale(mGrey, faces);
+        System.out.println(String.format("Detected %s faces ---------  %s", faces.toArray().length,i));
+        i++;
+        for(Rect rect:faces.toArray())
+        {
+            Point center= new Point(rect.x + rect.width*0.5, rect.y + rect.height*0.5 );
+            Core.ellipse( mRgba, center, new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 255, 0, 255 ), 4, 8, 0 );
+        }*/
+		if(first){
+			mGray.copyTo(previous);
+			first = false;
+		}
+
+		Core.absdiff(mGray, previous, mDiff);
+		Imgproc.threshold(mDiff,mDiff,SENSITIVITY_VALUE,255,Imgproc.THRESH_BINARY);
+		Imgproc.blur(mDiff,mDiff, new Size(5,5));
+		Imgproc.threshold(mDiff,mDiff,SENSITIVITY_VALUE,255,Imgproc.THRESH_BINARY);
+		mGray.copyTo(previous);
+
+
+
+		mDiff.copyTo(mCont);
+		java.util.List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Imgproc.findContours(mCont, contours,new Mat(), Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_TC89_L1);
+
+		Imgproc.cvtColor(mDiff, mDiff, Imgproc.COLOR_GRAY2BGR);
+		System.out.println("Contours " + Integer.toString(contours.size()));
+		if (contours.size()>0)
+		{
+			Rect objectBoundingRectangle = boundingRect(contours.get(contours.size()-1));
+			int x = objectBoundingRectangle.x;//+objectBoundingRectangle.width/2;
+			int y = objectBoundingRectangle.y;//+objectBoundingRectangle.height/2;
+
+			//Core.rectangle(mDiff, new Point(x,y), new Point(x+objectBoundingRectangle.width, y+objectBoundingRectangle.height), new Scalar(0,255,0), 1);
+			//Core.line(mDiff, new Point(x,y-25), new Point(x, y+25), new Scalar(0,255,0), 1);
+		}
+
+		return mDiff;
+	}
 }
